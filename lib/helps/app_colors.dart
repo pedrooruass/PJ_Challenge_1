0832beb321269fb1cf41ffc35c01679538e9407a
Pedro Ruas <ruasp0509@gmail.com>
2021-10-20 07:41:28 -0500

    add orange color

---
+++ lib/helps/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // satatic: chamar sem construtor,
+  // get: smp usar "=>"; msma coisa de usar o "=" - primaryColor = Color(0xFF3D78E3).
+  // blue
+  static get primaryColor => Color(0xFF3D78E3);
+  // orange
+  static get secondColor => Color(0xFFFFC85B);
+}
