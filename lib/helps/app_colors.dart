529d25a7d13d1b24ac2ff510ca5786593231a1db
Pedro Ruas <ruasp0509@gmail.com>
2021-10-24 15:01:41 -0500

    Change Colors

--- lib/helps/app_colors.dart
+++ lib/helps/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   // satatic: chamar sem construtor,
   // get: smp usar "=>"; msma coisa de usar o "=" - primaryColor = Color(0xFF3D78E3).
   // blue
-  static get primaryColor => Color(0xFF3D78E3);
+  static get primaryColor => Color(0xFF0C56CA);
   // orange
-  static get secondColor => Color(0xFFFFC85B);
+  static get secondColor => Color(0xFFEB7653);
 }
